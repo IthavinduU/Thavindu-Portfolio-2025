572bd57efc523a2adf8d0f01955b39509bd2cccc
IthavinduU <thavindul@gmail.com>
2025-06-01 19:59:28 +0530

    icon fixes

--- src/components/ui/resizable-navbar.tsx
+++ src/components/ui/resizable-navbar.tsx
@@ -237,7 +237,7 @@ export const NavbarLogo = () => {
       className="relative z-20 mr-4 flex items-center space-x-2 px-2 py-1 text-sm font-normal text-black"
     >
       <img
-        src="https://avatars.githubusercontent.com/u/135231247?v=4"
+        src="icon.png"
         className="rounded-full"
         alt="logo"
         width={30}
