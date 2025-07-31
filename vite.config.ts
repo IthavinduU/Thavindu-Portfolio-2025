ef532f98b258544c8907b4b816ad9f1f12c22048
Thavindu Liyanage <thavindul@gmail.com>
2025-07-31 18:11:13 +0530

    Config updates -> .live

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import sitemap from 'vite-plugin-sitemap';  // <-- default import
+import sitemap from 'vite-plugin-sitemap';  
 
 export default defineConfig({
   plugins: [
     react(),
     sitemap({
-      hostname: 'https://thavindu.me',
+      hostname: 'https://thavindu.live', 
       routes: ['/', '/about', '/projects', '/services', '/roadmap', '/articles', '/contact'],
     }),
   ],
